39ee5870edc2acbfa396c40e29d235fdf39fa46b
Shawn <spapineau@spaptechnology.com>
2026-06-01 21:51:56 -0400

    Match AuthKit token audience in Convex

--- client/convex/auth.config.ts
+++ client/convex/auth.config.ts
@@ -21,6 +21,7 @@ const authConfig = {
       issuer: `https://api.workos.com/user_management/${clientId}`,
       algorithm: "RS256",
       jwks: `https://api.workos.com/sso/jwks/${clientId}`,
+      applicationID: clientId,
     },
   ],
 };
